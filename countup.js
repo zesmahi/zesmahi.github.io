24df2e48f7053892aeb8e6ced44533591885b2e8
zesmahi <86168080+zesmahi@users.noreply.github.com>
2022-02-01 21:35:26 -0700

    timed div reveals
    also new timer

--- countup.js
+++ countup.js
@@ -1,27 +1,9 @@
-function newtime(){
-	var countDownDate = localStorage.getItem('startDate'); 
-	if (countDownDate) { 
-		countDownDate = new Date(countDownDate); 
-	} else { 
-		countDownDate = new Date(); 
-		localStorage.setItem('startDate', countDownDate); 
-	} 
-	// Update the count down every 1 second 
-	var x = setInterval(function() { 
- 
-		// Get todays date and time 
-		var now = new Date().getTime(); 
- 
-		// Find the distance between now an the count down date 
-		var distance = now - countDownDate.getTime(); 
-  
-		// Time calculations for days, hours, minutes and seconds 
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24)); 
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); 
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000); 
-     
-		// Output the result in an element with id="timer" 
-		document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "; 
-}, 1000); 
+var seconds = 0;
+var timer = document.getElementById("seconds-counter");
+
+function incrementSeconds() {
+    seconds += 1;
+    timer.innerText = "You have kept me alive for " + seconds + " second(s).";
 }
+
+var increment = setInterval(incrementSeconds, 1000);
---
+++ timedrevealer.js
@@ -0,0 +1,14 @@
+function showthankyou() {
+    document.getElementById("thankyou").style.visibility = "visible";
+}
+setTimeout("showthankyou()", 10000); // after 10 sec
+
+function showgoodperson() {
+    document.getElementById("goodperson").style.visibility = "visible";
+}
+setTimeout("showgoodperson()", 30000); // after 30 secs
+
+function showimpatiencemodule() {
+    document.getElementById("impatiencemodule").style.visibility = "visible";
+}
+setTimeout("showimpatiencemodule()", 60000); // after 60 secs
