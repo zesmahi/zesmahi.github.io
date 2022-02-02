3817004708a890f02830b347a7912d8cee5b9055
zesmahi <86168080+zesmahi@users.noreply.github.com>
2022-02-01 22:20:55 -0700

    impressed by impatience rating
    wow!

--- impatience.js
+++ impatience.js
@@ -1,4 +1,9 @@
 var impatiencerating = 0;
+function newthings1() {
+	if (impatiencerating >= 25) {
+		document.getElementById("newthingstoread1").style.visibility = "visible";
+	}
+}
 function increment_impatience(){
 	impatiencerating = impatiencerating + 1;
 	impatiencecounter.innerHTML = impatiencerating;
