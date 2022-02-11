d454ab01e9790752c8d4ecf68b8a94e906d960ee
zesmahi <86168080+zesmahi@users.noreply.github.com>
2022-02-11 16:46:32 -0700

    main
    main

--- README.md
+++
@@ -1,5 +0,0 @@
-# zesmahi.github.io
-website.
-dumb project, i hope you think about it a little bit sometimes.
-i also hope you dont think about it too much.
-i am sorry if you do think about it too much.
--- countup.js
+++
@@ -1,9 +0,0 @@
-var seconds = 0;
-var timer = document.getElementById("seconds-counter");
-
-function incrementSeconds() {
-    seconds += 1;
-    timer.innerText = "You have kept me alive for " + seconds + " second(s).";
-}
-
-var increment = setInterval(incrementSeconds, 1000);
